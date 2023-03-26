29e29da8e581de6d9178fbce253c595843d8bccc
Yegor Kazantsev <wwwadmin@bk.ru>
2023-03-26 23:20:18 +0300

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 import argparse
-import pathlib
-import sqlite3
+from repositories.image import Sqllite3ImageRepository
 
 from dotenv import load_dotenv
+
 load_dotenv()
-
-
-def _normalization_db_filename(db_file: str) -> str:
-    if not db_file.endswith(".db") or not db_file.endswith(".sqllite") or not db_file.endswith(".sqllite3"):
-        db_file = db_file + ".sqllite3"
-    return db_file
 
 
 def main():
@@ -35,24 +29,11 @@ def main():
     )
     args = parser.parse_args()
     db_file: str = args.db[0]
-    db_file = _normalization_db_filename(db_file)
-    try:
-        sqlite_connection = sqlite3.connect(db_file)
-    except sqlite3.Error as e:
-        print(f"Error on open database file: {e}")
-        quit()
-    finally:
-        if (sqlite_connection):
-            sqlite_connection.close()
-            print("Соединение с SQLite закрыто")
-    cursor = sqlite_connection.cursor()
-    print("База данных создана и успешно подключена к SQLite")
 
-    sqlite_select_query = "select sqlite_version();"
-    cursor.execute(sqlite_select_query)
-    record = cursor.fetchall()
-    print("Версия базы данных SQLite: ", record)
-    cursor.close()
+    image_db = Sqllite3ImageRepository(db_file)
+
+
+
 
 if __name__ == "__main__":
     main()
---
+++ repositories/image.py
@@ -0,0 +1,61 @@
+import pathlib
+import sqlite3
+
+
+class Sqllite3ImageRepository():
+    _default_ext = ".sqllite3"
+    _allowed_sqllite_file_ext = [".sqllite",
+                                 ".db",
+                                 ".sqllite3"
+                                 ]
+    _image_db_table = "image_db"
+
+    def _normalization_db_filename(self, db_file: str) -> str:
+        has_allowed_ext: bool = False
+        for ext in self._allowed_sqllite_file_ext:
+            if db_file.endswith(ext):
+                has_allowed_ext = True
+        if not has_allowed_ext:
+            db_file = f"db_file{self._default_ext}"
+        return db_file
+
+    def __init__(self, db_file: str):
+        try:
+            self._connection: sqlite3.Connection = sqlite3.connect(db_file)
+        except sqlite3.Error as e:
+            if not self._connection:
+                return
+            self._connection.close()
+            print(f"Error on open database file: {e}")
+            quit()
+        cursor = self._connection.cursor()
+        print("База данных создана и успешно подключена к SQLite")
+
+        sqlite_select_query = "select sqlite_version();"
+        cursor.execute(sqlite_select_query)
+        record = cursor.fetchall()
+        print("Версия базы данных SQLite: ", record)
+        sql: str = "select 'table' from sqlite_master where type = 'table'"
+        cursor.execute(sql)
+        tables = cursor.fetchall()
+        print(tables)
+        if self._image_db_table not in tables:
+            self._create_db()
+
+        cursor.close()
+
+    def _create_db(self):
+        cursor = self._connection.cursor()
+        sql_create_image_db:str = f"""create table {self._image_db_table}
+        (
+            path   TEXT,
+            width  integer,
+            height integer,
+            size   integer,
+            hash   TEXT
+        );
+        """
+        cursor.execute(sql_create_image_db)
+        sql_index_image_db:str = f"create index {self._image_db_table}_hash_index on image_db(hash);"
+        cursor.execute(sql_index_image_db)
+        cursor.close()
